e4a35b6e0db13caf00ed537cb057daa6c15d0749
RevathiLokesh <revathilokesh19@gmail.com>
2021-12-07 11:59:18 +0545

    Password is checked

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -2,4 +2,12 @@ package org.test;
 
 public class Test {
 
+	public static void password()
+	{
+		System.out.println("Password is checked");
+	}
+	public static void main(String[] args)
+	{
+		password();
+	}
 }
