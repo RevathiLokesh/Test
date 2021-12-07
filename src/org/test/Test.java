632f01a78bc68a983afdc1d0be5d4c6744dd5975
RevathiLokesh <revathilokesh19@gmail.com>
2021-12-07 11:54:06 +0545

    Username is checked

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -1,5 +1,14 @@
 package org.test;
 
 public class Test {
+ 
+	public static void username()
+	{
+		System.out.println("Username is created");
+	}
+	public static void main(String[] args)
+	{
+		username();
+	}
+	}
 
-}
